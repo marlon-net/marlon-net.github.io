52729f5d5aa177d1e7071f1216f60ad5de725a01
Marlon Dias <marlon@marlon.net>
2015-05-31 20:13:17 -0300

    Added index.html

---
+++ instala1.bat
@@ -0,0 +1,12 @@
+echo # marlon-net.github.io >> README.md
+pause
+git init
+pause
+git add README.md
+pause
+git commit -m "first commit"
+pause
+git remote add origin git@github.com:marlon-net/marlon-net.github.io.git
+pause
+git push -u origin master
+
